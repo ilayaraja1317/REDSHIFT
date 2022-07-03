93568a274f77d4ffe6e496212adb9183b6a8690c
“ilayaraja1317” <ilayaraja1317@gmail.com>
2022-07-03 05:57:09 +0000

    table create commit

--- test.sql
+++ test.sql
@@ -1,1 +1,7 @@
-select * from users;
+ create table sales (
+    id bigint identity (1, 1) not null,
+    email varchar(100) not null,
+    product_id int not null,
+    quantity int not null,
+    date_created timestamp
+)
