1e8f05d5875d138cb692e9047ec3a0c2c7a87fa9
ilayaraja1317 <101242493+ilayaraja1317@users.noreply.github.com>
2022-07-09 16:19:23 +0530

    Update branch_pipeline.py

--- branch_pipeline.py
+++ branch_pipeline.py
@@ -6,8 +6,7 @@ def main():
    print(input2)
 if __name__ == "__main__":
   print('Welcome to Redshift Deployment')
-  main()
-
+  
 if ((input2=='DEV') and (input1=='main')):
  { 
   print('It is DEV ENV & main brach')
@@ -17,3 +16,4 @@ else:
  {
   print('Its not a DEV & main brach')
   }
+main()
