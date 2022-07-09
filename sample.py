7bc087364170d9771a4f34e310fc4eb0e7fcc908
ilayaraja1317 <101242493+ilayaraja1317@users.noreply.github.com>
2022-07-09 14:46:31 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,10 @@
-print('Welcome to Redshift Deployment')
+import sys
+
+def main():
+   input1 = sys.argv[1]
+   input2 = sys.argv[2]
+
+if __name__ == "__main__":
+  print('Welcome to Redshift Deployment')
+   main()
+
