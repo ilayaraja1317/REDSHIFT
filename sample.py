0e8f1c70c0e402304a32c7585d0f2b545273d193
ilayaraja1317 <101242493+ilayaraja1317@users.noreply.github.com>
2022-07-09 14:47:50 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,7 +3,8 @@ import sys
 def main():
    input1 = sys.argv[1]
    input2 = sys.argv[2]
-
+   print(input1)
+   print(input2)
 if __name__ == "__main__":
   print('Welcome to Redshift Deployment')
    main()
