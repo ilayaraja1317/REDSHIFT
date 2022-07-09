b5cea377b9dfe654c4faf7817929a6d7f6107c3d
ilayaraja1317 <101242493+ilayaraja1317@users.noreply.github.com>
2022-07-09 18:06:16 +0530

    Update branch_pipeline.py

--- branch_pipeline.py
+++ branch_pipeline.py
@@ -1,4 +1,5 @@
 import os
+import contest3.py
 def main():
    input1 =os.environ['Branch']
    input2 =os.environ['ENV']
@@ -6,10 +7,11 @@ def main():
    print(input2)
    if (input2=='DEV'):
       print('It is DEV ENV & main brach')
-      execfile("contest2.py")
+      contest3.rs_dep()
       print('Deploy completed')
    else:
       print('Its not a DEV & main brach')
 if __name__ == "__main__":
    print('Welcome to Redshift Deployment')
    main()
+   
